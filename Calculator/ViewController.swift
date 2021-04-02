286c067e4a806c8bcfc14dc46f8b056e1ffc71d1
Dawid Jóźwiak <dawidjoz@wp.pl>
2021-04-02 23:55:45 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dawid Jóźwiak on 4/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
